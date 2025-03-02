743001b9bfa7b5ff00d535c92547c63c8fa6bc50
Caioba <83611625+CaioC29@users.noreply.github.com>
2025-03-02 01:33:54 -0300

    v1.1
    - Adicionada a opção de escolher formatos de vídeo (.mp4 | .mov | .webm)
    - Adicionada a opção de escolher formatos de áudio (.mp3 | .wav | .opus)
    - Melhorias na interface dos menus

--- Script.bat
+++ Script.bat
@@ -1,15 +1,17 @@
 @echo off
 
-title Script de automacao para YT-DLP By Caioba
+title Script YT-DLP By Caioba
 chcp 65001 > nul
 
 :menu
-echo ===== SCRIPT DE AUTOMAÇÃO PARA YT-DLP =====         By: Caioba
+cls
+echo ===== SCRIPT DE AUTOMAÇÃO PARA YT-DLP (v1.1) =====         By: Caioba
 echo.
 echo.
-echo (1) Baixar vídeo em qualidade máxima
-echo (2) Baixar somente áudio
-echo (0) Sair
+echo [1] Baixar vídeo
+echo [2] Baixar áudio
+echo.
+echo [0] Sair
 echo.
 echo.
 set /p opc=Insira uma opção: 
@@ -20,7 +22,7 @@ if %opc% == 0 goto sair
 
 ::opc invalida
 echo.
-echo Opção inválida, retornando ao menu
+echo Opção inválida, tente novamente
 timeout /t 3
 echo.
 cls
@@ -28,18 +30,130 @@ goto menu
 
 
 :video
+cls
+echo ===== SCRIPT DE AUTOMAÇÃO PARA YT-DLP (v1.1) =====         By: Caioba
+echo.
+echo.
+echo [✓] Baixar vídeo [SELECIONADO]
+echo.
+echo Escolha o formato:
+echo.
+echo [1] .mp4
+echo [2] .mov
+echo [3] .webm
+echo.
+echo [0] Voltar ao menu
+echo.
+echo.
+set /p opc=Insira uma opção: 
+if %opc% == 1 goto mp4
+if %opc% == 2 goto mov
+if %opc% == 3 goto webm
+if %opc% == 0 goto menu 
+
+
+::opc invalida
+echo.
+echo Opção inválida, tente novamente
+timeout /t 3
+echo.
+cls
+goto video
+
+
+:mp4
 echo.
 set /p url=Insira a URL: 
 echo.
 echo Iniciando download...
 echo.
-yt-dlp.exe -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" %url%
+yt-dlp.exe --recode mp4 %url%
+echo.
+pause
+exit
+
+
+:mov
+echo.
+set /p url=Insira a URL: 
+echo.
+echo Iniciando download...
+echo.
+yt-dlp.exe --recode mov %url%
+echo.
+pause
+exit
+
+
+:webm
+echo.
+set /p url=Insira a URL: 
+echo.
+echo Iniciando download...
+echo.
+yt-dlp.exe %url%
 echo.
 pause
 exit
 
 
 :audio
+cls
+echo ===== SCRIPT DE AUTOMAÇÃO PARA YT-DLP (v1.1) =====         By: Caioba
+echo.
+echo.
+echo [✓] Baixar áudio [SELECIONADO]
+echo.
+echo Escolha o formato:
+echo.
+echo [1] .mp3
+echo [2] .wav
+echo [3] .opus
+echo.
+echo [0] Voltar ao menu
+echo.
+echo.
+set /p opc=Insira uma opção: 
+if %opc% == 1 goto mp3
+if %opc% == 2 goto wav
+if %opc% == 3 goto opus
+if %opc% == 0 goto menu 
+
+
+::opc invalida
+echo.
+echo Opção inválida, tente novamente
+timeout /t 3
+echo.
+cls
+goto audio
+
+
+:mp3
+echo.
+set /p url=Insira a URL: 
+echo.
+echo Iniciando download...
+echo.
+yt-dlp.exe -x --recode mp3 %url%
+echo.
+pause
+exit
+
+
+:wav
+echo.
+set /p url=Insira a URL: 
+echo.
+echo Iniciando download...
+echo.
+yt-dlp.exe -x --recode wav %url%
+echo.
+pause
+exit
+
+
+:opus
 echo.
 set /p url=Insira a URL: 
 echo.
@@ -54,6 +168,8 @@ exit
 :sair
 cls
 color 04
-echo Tchau
-timeout /t 5 /nobreak
+echo.
+echo Saindo...
+echo.
+timeout /t 3 /nobreak
 exit
